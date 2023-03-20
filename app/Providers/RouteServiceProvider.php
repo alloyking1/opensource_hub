b868795072394b8d6744235670ec0b21a739fbab
alloyking1 <alloyking1@gmail.com>
2023-03-20 17:43:06 +0100

    modified route service provider to add an auth middleware to the projects route created a seeder class for users

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
 
-            Route::middleware('web')
+            Route::middleware(['web', 'auth'])
                 ->prefix('project')
                 ->group(base_path('routes/project.php'));
         });
