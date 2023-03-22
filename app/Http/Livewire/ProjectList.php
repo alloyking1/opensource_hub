58a45d4250b03c96e35c1315a79531aa79ce22bf
alloyking1 <alloyking1@gmail.com>
2023-03-22 15:14:11 +0100

    implemented infinite scrolling to projects

--- app/Http/Livewire/ProjectList.php
+++ app/Http/Livewire/ProjectList.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Project;
 use Livewire\Component;
 
 class ProjectList extends Component
 {
+    public $pages = 10;
+
+    public function loadMore()
+    {
+        $this->pages += 10;
+    }
+
     public function render()
     {
-        return view('livewire.project-list');
+        return view('livewire.project-list', [
+            'projects' =>  Project::paginate($this->pages),
+        ]);
     }
 }
--- resources/views/livewire/project-list.blade.php
+++ resources/views/livewire/project-list.blade.php
@@ -1,4 +1,28 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
-    <h1>hello world</h1>
+    @foreach ($projects as $project )
+        <h1>{{ $project->name }}</h1> 
+        <p>{{ $project->description }}</p>
+        <p>{{ Str::substr($project->body,0, 100) }}</p>
+
+        <hr>
+    @endforeach
+    <div
+    x-data="{
+        observe () {
+            let observer = new IntersectionObserver((entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        @this.call('loadMore')
+                    }
+                })
+            }, {
+                root: null
+            })
+ 
+            observer.observe(this.$el)
+        }
+    }"
+    x-init="observe">
+</div>
+    <button wire:click.prevent="loadMore" class="p-4 bg-blue-500 text-white">Load more</button>
 </div>
