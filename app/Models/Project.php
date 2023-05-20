59646651a5db2141ebb341193bc07c9ea8bd571a
alloyking1 <alloyking1@gmail.com>
2023-05-20 14:18:13 +0100

    created like table and added like and dislike function in project controller

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Like;
 
 class Project extends Model
 {
@@ -15,5 +16,26 @@ class Project extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    //$user->liked($post)
+    public function like($user = null, $liked = true)
+    {
+        $this->likes()->updateOrCreate([
+            'user_id' => $user ? $user->id : auth()->id(),
+        ], [
+            'liked' => $liked,
+        ]);
+    }
+
+    public function dislike($user = null)
+    {
+        $this->like($user, false);
+    }
+
+    //like relationship
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
